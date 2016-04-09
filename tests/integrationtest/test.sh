62ff5346b4bfb6f20857cf95d45aa3a74b10f636
Johan Bjäreholt <johanthepro@gmail.com>
2016-04-09 18:05:43 +0200

    Added simple integration test

---
+++ tests/integrationtest/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# This is essentially just a crash test, not a proper unit test
+#
+
+gtestpass=0
+gtestcount=0
+
+EXEC=../../compiler
+
+function itest() {
+	gtestcount=$(($gtestcount+1))
+	echo "Integration test: $1"
+	cat $1 | $EXEC
+	if [ $? -eq 0 ]; then
+		gcc program.s ../../libs/* -o program
+		./program
+		if [ $? -eq 0 ]; then
+			echo "Passed $1"
+			gtestpass=$(($gtestpass+1))
+		fi
+	else
+		echo "FAILED: compilation of $1"
+		failedtests=$failedtests$1
+	fi
+}
+
+itest "tests/basic.src"
+
+printf "%d/%d tests passed\n" $gtestpass $gtestcount
